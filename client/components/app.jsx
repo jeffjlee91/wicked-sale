b8091036091a1dbd3c5e5636f708ac2d8d16256e
Jeff Lee <jeffjlee91@yahoo.com>
2019-11-21 17:30:40 -0800

    View cart summary front end -Jeff Lee

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './header';
 import ProductList from './product-list';
 import ProdcutDetails from './product-details';
+import CartSummary from './cart-summary';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -62,20 +63,23 @@ export default class App extends React.Component {
   render() {
     if (this.state.view.name === 'catalog') {
       return (<div>
-        <Header cartItemCount={this.state.cart.length} ></Header>
+        <Header cartItemCount={this.state.cart.length} setView={this.setView}></Header>
         <ProductList setView={this.setView}/>
       </div>);
     } else if (this.state.view.name === 'details') {
       return (<div>
-        <Header cartItemCount={this.state.cart.length}></Header>
+        <Header cartItemCount={this.state.cart.length} setView={this.setView}></Header>
         <ProdcutDetails
           product={this.state.view.params}
           setView={this.setView}
           addToCart={this.addToCart}/>
       </div>
-
       );
+    } else if (this.state.view.name === 'cart') {
+      return <div>
+        <Header cartItemCount={this.state.cart.length}></Header>
+        <CartSummary props={this.state.cart} setView={this.setView}></CartSummary>
+      </div>;
     }
-
   }
 }
